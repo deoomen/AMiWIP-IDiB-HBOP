b9f2a6a61855af595783c620ab42c587568fef19
deoomen <deoomen@users.noreply.github.com>
2017-05-30 20:24:39 +0200

    remove unnecessary files

--- 1_sensor-Arduino/README.md
+++
@@ -1,5 +0,0 @@
-# Komunikacja bezprzewodowa czujnik - Arduino
-
-* ESP - czujnik - bateryjka
-* komunikacja po wifi co interwał czasu
-
--- 3_cloud-web/config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-    lastFeeds: 10,
-    TSread: {
-        0: { ApiKey: "ERR358N3PQ5HLMWY", ChannelID: "278694" },
-        1: { ApiKey: "R0L17Y04SR3YS6AW", ChannelID: "278703" }
-    }
-};
